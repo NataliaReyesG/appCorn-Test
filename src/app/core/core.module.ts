9bd0d972d9f1b7f74d9c95a4f85072b014e21b47
NataliaReyesG <natalia.arg9@gmail.com>
2022-01-20 15:28:47 -0500

    1 binaries uploaded

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,10 +5,17 @@ import { SideMenuComponent } from './side-menu/side-menu.component';
 import { NgMaterialModule } from './ng-material.module';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   imports: [CommonModule, NgMaterialModule, RouterModule],
-  declarations: [NavbarComponent, SideMenuComponent, FooterComponent],
-  exports: [NavbarComponent, NgMaterialModule, SideMenuComponent, FooterComponent]
+  declarations: [NavbarComponent, CarouselComponent, SideMenuComponent, FooterComponent],
+  exports: [
+    NavbarComponent,
+    CarouselComponent,
+    NgMaterialModule,
+    SideMenuComponent,
+    FooterComponent,
+  ],
 })
 export class CoreModule {}
