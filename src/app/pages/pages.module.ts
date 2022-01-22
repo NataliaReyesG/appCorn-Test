a71714bdb4e1c3705bfbb049d8c267944d0dbecb
NataliaReyesG <natalia.arg9@gmail.com>
2022-01-22 15:08:01 -0500

    1 binaries uploaded

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
 import { HowComponent } from './how/how.component';
+import { WhatComponent } from './what/what.component';
 import { TeamComponent } from './team/team.component';
 import { ProductsComponent } from './products/products.component';
 import { NgMaterialModule } from '../core/ng-material.module';
@@ -10,7 +11,7 @@ import { NgMaterialModule } from '../core/ng-material.module';
 
 @NgModule({
   imports: [CommonModule, PagesRoutingModule, NgMaterialModule],
-  declarations: [HomeComponent,HowComponent, TeamComponent, ProductsComponent],
-  exports: [ProductsComponent, HomeComponent, HowComponent, TeamComponent, ProductsComponent]
+  declarations: [HomeComponent,HowComponent, WhatComponent, TeamComponent, ProductsComponent],
+  exports: [ProductsComponent, HomeComponent,WhatComponent, HowComponent, TeamComponent, ProductsComponent]
 })
 export class PagesModule {}
